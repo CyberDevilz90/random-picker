c094a1646d0973990c580164d446062f8d321ea2
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-03-04 13:49:13 +0700

    Random Picker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,122 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
+    data() {
+        return {
+            names: [],
+            state: true,
+            inputName: "",
+            showError: false,
+            errorMsg: "",
+            result: "",
+        };
+    },
+    computed: {
+        isReady() {
+            return this.names.length > 1;
+        },
+    },
+    methods: {
+        addNameToList() {
+            if (this.validate(this.inputName)) {
+                this.names.push(this.inputName);
+                this.inputName = "";
+            } else {
+                this.showError = true;
+            }
+        },
+        validate(value) {
+            this.errorMsg = "";
+            if (value === "") {
+                this.errorMsg = "Please enter a valid Name";
+                return false;
+            }
+            if (this.names.includes(value)) {
+                this.errorMsg = "Name already taken";
+                return false;
+            } else {
+                return true;
+            }
+        },
+        removeName(i) {
+            this.names.splice(i, 1);
+        },
+        changeState() {
+            this.result = this.getRandomNameFromList();
+            if (this.result !== "") {
+                const index = this.names.indexOf(this.result);
+                if (index !== -1) {
+                    this.names.splice(index, 1);
+                }
+            }
+            this.state = false;
+        },
+        getRandomNameFromList() {
+            return this.names[Math.floor(Math.random() * this.names.length)]; 
+        },
+        newRandom() {
+            this.names = [];
+            this.state = true;
+        },
+        newResult() {
+            if (this.names.length > 1) {
+                let availableNames = this.names.filter(
+                    (name) => name !== this.result
+                ); 
+                this.result = this.getRandomNameFromList(availableNames); 
+            } else if (this.names.length === 1) {
+                this.result = this.names[0]; 
+            } else {
+                this.result = "";
+            }
+						this.names.splice(this.names.indexOf(this.result),1)
+						if (this.names.length == 0) {
+							this.newRandom()
+						}
+        },
+    },
+};
 </script>
 
+<template>
+    <div class="hello" v-if="state">
+			<h1 class="title">Random Picker</h1>
+        <div class="inputContainer">
+            <input class="input" type="text" v-model.trim.lazy="inputName" />
+            <button class="addButton" @click="addNameToList">Add</button>
+        </div>
+        <div class="errorLabel" v-if="showError">
+            {{ errorMsg }}
+        </div>
+				<div class="listNamesContainer">
+					<p
+            class="listNames"
+            v-for="(name, index) in names"
+            :key="name"
+            @click="removeName(index)"
+        >
+            {{ name }}
+        </p>
+				</div>
+       
+        <div class="checkButton" v-if="isReady" @click="changeState">
+            Check the Winner
+        </div>
+    </div>
+    <div class="resultContainer" v-if="!state">
+        <h1 class="title">Result</h1>
+        <p class="listNames">{{ result }}</p>
+        <div class="checkButton" @click="newResult">Random Again</div>
+        <div class="checkButton" @click="newRandom">
+            Create New Random Picker
+        </div>
+    </div>
+</template>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style >
+@import url("../assets/style/HelloWorld.css");
 </style>
